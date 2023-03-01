ef4c70f7bb3e13604d4acb29fe8039b09b1c3a85
William Simas <dev.wb10@gmail.com>
2023-03-01 15:49:32 -0300

    att

--- vetor3.py
+++ vetor3.py
@@ -1,18 +1,21 @@
 import json
 
-with open('faturamento.json', 'r') as f:
+with open('dados.json', 'r') as f:
     faturamento_diario = json.load(f)
 
+valores_faturamento = [d.get('faturamento', 0) for d in faturamento_diario]
 
-menor_faturamento = min(faturamento_diario)
-maior_faturamento = max(faturamento_diario)
+menor_faturamento = min(valores_faturamento)
+maior_faturamento = max(valores_faturamento)
 
-faturamento_sem_zero = [f for f in faturamento_diario if f != 0]
-media_faturamento = sum(faturamento_sem_zero) / len(faturamento_sem_zero)
+faturamento_sem_zero = [f for f in valores_faturamento if f != 0]
 
+if len(faturamento_sem_zero) > 0:
+    media_faturamento = sum(faturamento_sem_zero) / len(faturamento_sem_zero)
+else:
+    media_faturamento = 0
 
 dias_acima_da_media = len([f for f in faturamento_sem_zero if f > media_faturamento])
-
 
 print(f'Menor faturamento diário: R$ {menor_faturamento:.2f}')
 print(f'Maior faturamento diário: R$ {maior_faturamento:.2f}')
